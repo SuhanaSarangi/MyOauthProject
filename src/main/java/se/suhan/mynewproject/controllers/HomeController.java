9760dfde5bb804aecff7bd982925b068a29536f9
Suhana Sarangi <hejsuhana@gmail.com>
2025-05-28 12:42:49 +0200

    added commit

--- src/main/java/se/suhan/mynewproject/controllers/HomeController.java
+++ src/main/java/se/suhan/mynewproject/controllers/HomeController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import se.suhan.mynewproject.models.BookRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class HomeController extends BaseController {
@@ -16,20 +14,13 @@ public class HomeController extends BaseController {
     // This is a placeholder for the HomeController class.
     // You can add methods and logic here to handle requests and responses.
     // For example, you might want to add a method to return the home page view.
-    // en controller är en klass som mappar
-    // HTTP-förfrågningar till specifika metoder i din applikation.
-    //funktionen ska då retunera HTML för den sidan
-
-    // Exempel:
+    // You can also add methods to handle specific routes or endpoints.
 
     @GetMapping("/")
     public String home(Model model) {
-        // Logik för att hantera hem-sidan
-        // Du kan returnera en vy eller JSON-data här
-        // Hämta saker från databasen eller annan källa
-        // List<String> books = bookService.getAllBooks();
+
         System.out.println("Home page accessed");
-        //List<String> books = new ArrayList<>();
+        // List<String> books = new ArrayList<>();
         //books.add("Book 1");
         //books.add("Book 2");
         //books.add("Book 3");
